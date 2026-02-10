e78ba0ee92b2c222fa9b3cbaf2cd7312aa9cd2d0
abilash <abilashsp07@gmail.com>
2026-02-10 13:41:49 +0530

    add api expose

--- src/functions/events.js
+++ src/functions/events.js
@@ -1,13 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-
 const supabase = createClient(
-supabaseUrl,
- supabaseKey
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
-
 export async function handler(event) {
   try {
     const params = event.queryStringParameters || {};
